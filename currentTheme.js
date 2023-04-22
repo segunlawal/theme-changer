278149d70780c31e986e1f0a013fc59d8e9fb69d
Segun <lawalsegun72@gmail.com>
2023-04-22 03:58:07 +0100

    refactor

--- currentTheme.js
+++ currentTheme.js
@@ -1,9 +1,9 @@
 (function () {
-  document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+  const html = document.querySelector("html");
+  html.style.filter = "invert(1) hue-rotate(180deg)";
 
-  let multimedia = document.querySelectorAll("img, picture, video");
-
-  multimedia.forEach((eachItem) => {
-    eachItem.style.filter = "invert(1) hue-rotate(180deg)";
+  const multimedia = document.querySelectorAll("img, picture, video");
+  multimedia.forEach((item) => {
+    item.style.filter = "invert(1) hue-rotate(180deg)";
   });
 })();
--- oppTheme.js
+++ oppTheme.js
@@ -1,9 +1,9 @@
 (function () {
-  document.querySelector("html").style.filter = "invert(0) hue-rotate(0deg)";
+  const html = document.querySelector("html");
+  html.style.filter = "invert(0) hue-rotate(0deg)";
 
-  let multimedia = document.querySelectorAll("img, picture, video");
-
-  multimedia.forEach((eachItem) => {
-    eachItem.style.filter = "invert(0) hue-rotate(0deg)";
+  const multimedia = document.querySelectorAll("img, picture, video");
+  multimedia.forEach((item) => {
+    item.style.filter = "invert(0) hue-rotate(0deg)";
   });
 })();
