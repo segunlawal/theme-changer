2bf16bbae367770546e7860d9803b75a56e18d1b
Segun <lawalsegun72@gmail.com>
2023-04-22 03:47:49 +0100

    store button value in localstorage

--- popup.js
+++ popup.js
@@ -2,11 +2,20 @@ const button = document.getElementById("toggle-btn");
 const checkbox = document.querySelector('input[type="checkbox"]');
 
 if (document.querySelector(".popup")) {
+  // retrieve the value of the checkbox from localStorage
+  const checkboxValue = localStorage.getItem("checkboxValue");
+  if (checkboxValue) {
+    checkbox.checked = JSON.parse(checkboxValue);
+  }
+
   button.addEventListener("click", toggleButton);
 }
 
 function toggleButton() {
-  checkbox.checked ? !checkbox.checked : checkbox.checked;
+  checkbox.checked = !checkbox.checked;
+
+  // store the value of the checkbox in localStorage
+  localStorage.setItem("checkboxValue", JSON.stringify(checkbox.checked));
 
   const fileName = !checkbox.checked ? "oppTheme.js" : "currentTheme.js";
   chrome.tabs.query({ active: true }, function (tabs) {
